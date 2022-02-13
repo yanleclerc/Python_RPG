02f2b0ec2144169b96d45b162fe537597d1625dd
yanleclerc <ea091041@ens.uqam.ca>
2022-02-12 21:14:32 -0500

    Ajout methode map

--- level.py
+++ level.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -9,6 +12,18 @@ class Level:
         # Sprite initialisation (visible et obstacles)
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        #Sprite 
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites])
+
 
     def run(self):
         #Update et dessiner le jeu
