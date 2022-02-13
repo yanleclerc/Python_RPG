19e7e53ef5aa45c1561edde506dcee6fe43d80f9
yanleclerc <ea091041@ens.uqam.ca>
2022-02-12 23:32:55 -0500

    debug

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -22,10 +23,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x,y),[self.visible_sprites])
-
+                    Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    self.player = Player((x,y),[self.visible_sprites], self.obstacles_sprites)
 
     def run(self):
         #Update et dessiner le jeu
         self.visible_sprites.draw(self.display_surface)
-        pass
+        self.visible_sprites.update()
+        debug(self.player.direction)
